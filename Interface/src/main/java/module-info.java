0a7f424b0434722f4fcdda15af03c4b0a573de00
Arthur Gatti <arthurgatti@unifesspa.edu.br>
2026-06-08 16:20:39 -0300

    Padrão

---
+++ Interface/src/main/java/module-info.java
@@ -0,0 +1,11 @@
+module com.example.interface {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+
+    opens com.example.interface to javafx.fxml;
+    exports com.example.interface;
+}
